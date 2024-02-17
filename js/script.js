9a8c0fefdc45afcc5d3f7eafb1b7606c077ecf5e
Muavia-khan <150890569+Muavia-khan@users.noreply.github.com>
2024-02-17 10:38:31 +0500

    webflow portfolio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var type = new Typed(".typing", {
+  strings: [
+    "",
+    "WebFlow Developer",
+    "WebFlow Designer",
+    "WebFlow Builder",
+    "Freelancer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
